36499669618dec0dee7d64b24f91adcf49e5df1f
kabourad <mikage1san@gmail.com>
2022-10-08 01:05:19 +0100

    donne dbss7

--- js/main.js
+++ js/main.js
@@ -8,6 +8,8 @@ const body=document.querySelector("body")
 const state=localStorage.getItem("tfado")
 var base_url = window.location.origin;
 const our_url = window.location.origin.includes("github") ? base_url+"/"+ "orion.design" : base_url
+console.log(base_url)
+console.log(our_url)
 
 
 
@@ -16,7 +18,7 @@ const swap=()=>{
 	if(img.src.includes("night.png"))
 	{
 		all.style.filter=""
-		img.src = our_url + "/assets/sun.png"
+		img.src = "assets/sun.png"
 		img.style.filter=""
 		me.style.filter=""
 		body.style.backgroundColor= "bisque"
@@ -28,7 +30,7 @@ const swap=()=>{
 		me.style.filter="invert(100%) hue-rotate(180deg)"
 		body.style.backgroundColor= "white"
 		
-		img.src = our_url + "/assets/night.png"
+		img.src = "assets/night.png"
 		img.style.filter="invert(100%) hue-rotate(180deg)"
 		localStorage.setItem("tfado", "dlam");
 	}
