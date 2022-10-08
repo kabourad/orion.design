3de0e4be37e6a29f8b916e91a5c723be1259ed3a
kabourad <mikage1san@gmail.com>
2022-10-08 01:00:35 +0100

    donne dbss7

--- js/main.js
+++ js/main.js
@@ -6,10 +6,9 @@ let me=document.querySelector("#art")
 const all=document.querySelector("html")
 const body=document.querySelector("body")
 const state=localStorage.getItem("tfado")
+var base_url = window.location.origin;
+const our_url = window.location.origin.includes("github") ? base_url+"/"+ "orion.design" : base_url
 
-
-
-console.log(img.src)
 
 
 const swap=()=>{
@@ -17,7 +16,7 @@ const swap=()=>{
 	if(img.src.includes("night.png"))
 	{
 		all.style.filter=""
-		img.src = "/assets/sun.png"
+		img.src = our_url + "/assets/sun.png"
 		img.style.filter=""
 		me.style.filter=""
 		body.style.backgroundColor= "bisque"
@@ -29,7 +28,7 @@ const swap=()=>{
 		me.style.filter="invert(100%) hue-rotate(180deg)"
 		body.style.backgroundColor= "white"
 		
-		img.src = "/assets/night.png"
+		img.src = our_url + "/assets/night.png"
 		img.style.filter="invert(100%) hue-rotate(180deg)"
 		localStorage.setItem("tfado", "dlam");
 	}
@@ -39,5 +38,6 @@ if(state=="dlam" || state=="nor")
 {
 	swap()
 }
+
 
 botona.addEventListener("click", swap)
